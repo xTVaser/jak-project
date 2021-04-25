8755365af77c627c8f233bb6b84f9680fff9907b
Tyler Wilding <xtvaser@gmail.com>
2021-04-25 18:01:38 -0400

    got something functional for changing config files

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -1,38 +1,47 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-btn to="/" text>
-          <v-img
-            alt="OpenGOAL Logo"
-            class="shrink mr-2"
-            contain
-            src="logo-text-colored.png"
-            transition="scale-transition"
-            width="150"
-          />
-        </v-btn>
-      </div>
+      <v-container fluid>
+        <v-row justify="center" align="center" class="navbar_main">
+          <v-btn to="/" text>
+              <v-img
+                alt="OpenGOAL Logo"
+                class="shrink mr-2"
+                contain
+                src="logo-text-colored.png"
+                transition="scale-transition"
+                width="150"
+              />
+            </v-btn>
 
-      <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
 
-      <v-btn to="/docs" text>
-        <span class="mr-2">API Docs</span>
-      </v-btn>
-      <v-btn to="/configs" text>
-        <span class="mr-2">Decompiler Configs</span>
-      </v-btn>
+          <v-btn to="/docs" text>
+            <span class="mr-2">API Docs</span>
+          </v-btn>
+          <v-btn to="/decompiler-configs" text>
+            <span class="mr-2">Decompiler Configs</span>
+          </v-btn>
 
-      <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/water111/jak-project"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Repository</span>
-        <v-icon>mdi-github</v-icon>
-      </v-btn>
+          <v-btn
+            href="https://github.com/water111/jak-project"
+            target="_blank"
+            text
+          >
+            <span class="mr-2">Repository</span>
+            <v-icon>mdi-github</v-icon>
+          </v-btn>
+
+          <!-- <v-checkbox class="pt-3"
+            v-model="$vuetify.theme.dark"
+            color="purple"
+            off-icon="mdi-theme-light-dark"
+            on-icon="mdi-theme-light-dark"
+          ></v-checkbox> -->
+        </v-row>
+      </v-container>
     </v-app-bar>
 
     <v-main>
--- docs/src/router/index.js
+++ docs/src/router/index.js
@@ -18,12 +18,12 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/Docs.vue')
   },
   {
-    path: '/configs',
-    name: 'Game Configs',
+    path: '/decompiler-configs',
+    name: 'Decompiler Configs',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/GameConfigs.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/DecompilerConfigs.vue')
   }
 ]
 
