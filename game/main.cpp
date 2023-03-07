eb8bb7eb0d26c43e320f5e16632c820c0b13044c
Tyler Wilding <xtvaser@gmail.com>
2023-03-07 18:23:39 -0500

    working for HTTP - NOT HTTPS

--- game/main.cpp
+++ game/main.cpp
@@ -5,6 +5,7 @@
 
 #define STBI_WINDOWS_UTF8
 
+#include <iostream>
 #include <string>
 
 #include "runtime.h"
@@ -14,6 +15,8 @@
 #include "common/util/os.h"
 #include "common/util/unicode_util.h"
 #include "common/versions.h"
+
+#include "curl/curl.h"
 
 #ifdef _WIN32
 extern "C" {
@@ -40,11 +43,64 @@ void setup_logging(bool verbose) {
   lg::initialize();
 }
 
+void test_curl() {
+  CURL* curl;
+  CURLcode res;
+
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+
+  curl = curl_easy_init();
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.com/");
+
+#ifdef SKIP_PEER_VERIFICATION
+    /*
+     * If you want to connect to a site who is not using a certificate that is
+     * signed by one of the certs in the CA bundle you have, you can skip the
+     * verification of the server's certificate. This makes the connection
+     * A LOT LESS SECURE.
+     *
+     * If you have a CA cert for the server stored someplace else than in the
+     * default bundle, then the CURLOPT_CAPATH option might come handy for
+     * you.
+     */
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+#endif
+
+#ifdef SKIP_HOSTNAME_VERIFICATION
+    /*
+     * If the site you are connecting to uses a different host name that what
+     * they have mentioned in their server certificate's commonName (or
+     * subjectAltName) fields, libcurl will refuse to connect. You can skip
+     * this check, but this will make the connection less secure.
+     */
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+#endif
+
+    /* Perform the request, res will get the return code */
+    res = curl_easy_perform(curl);
+    /* Check for errors */
+    if (res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+
+    /* always cleanup */
+    curl_easy_cleanup(curl);
+  }
+
+  curl_global_cleanup();
+}
+
 /*!
  * Entry point for the game.
  */
 int main(int argc, char** argv) {
   ArgumentGuard u8_guard(argc, argv);
+
+  // TODO - you will have issues hitting HTTPS URLs
+  // we'll need to wrangle the SSL dependency
+  // see for an example
+  // - https://sourcegraph.com/github.com/RPCS3/rpcs3/-/blob/3rdparty/curl/CMakeLists.txt?L11:15
+  test_curl();
 
   // TODO - replace with CLI11 and just propagate args through
   // - https://github.com/CLIUtils/CLI11/issues/744
